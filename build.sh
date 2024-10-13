9797e4a88f6c1d37904eeef2321cf116951ebaf6
y-a-h-o-o <wuwuwahoo@gmail.com>
2024-10-13 12:16:23 -0400

    static linking

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
- g++ -O3 test.cpp -o test.exe -lGdi32 -lGdiplus -mwindows
+ g++ -O3 test.cpp -o test.exe -lGdi32 -lGdiplus -mwindows -static -static-libgcc -static-libstdc++
  echo "Program Compiled"
  exit 0
